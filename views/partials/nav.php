dda3d8d38555f9d96e7e3bfaa8ad07a1c3228f9a
laurenceschellevisch <laurence-aart@Hotmail.com>
2017-11-02 23:53:12 +0100

    last few changes

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,7 +1,7 @@
 <nav class="fixed-nav sticky-footer bg-dark" style="margin-bottom: 5%;" id="page-top">
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+    <a class="navbar-brand" href="index.html">ToDoApp</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
